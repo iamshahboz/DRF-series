547221aed6c02c6485adbd35ffa6996db327f2ae
Shahboz <sharafzodashahboz@gmail.com>
2025-01-11 02:28:27 +0500

    management for adding data to db

--- application/core/models.py
+++ application/core/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='/products/', blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
 
     @property
     def in_stock(self):
@@ -39,7 +39,16 @@ class Order(models.Model):
         return f'Order {self.order_id} by {self.user.username}'
     
 class OrderItem(models.Model):
-    pass 
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.order_id}"
 
 
 
