1e59a73c03b7e7ad03bcf91de1700bc9a51c99ce
Shahboz <sharafzodashahboz@gmail.com>
2025-02-02 20:20:28 +0500

    jwt token authentication url added

--- application/application/urls.py
+++ application/application/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include 
+from django.contrib.auth import views as auth_views
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('',include('core.urls')),
+    path('admin/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), #new
     path('admin/', admin.site.urls),
     path('silk/', include('silk.urls', namespace='silk')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
