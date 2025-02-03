52874d179f347b44278c889dbb6ec73b75c70157
Shahboz <sharafzodashahboz@gmail.com>
2025-02-03 06:19:14 +0500

    Pharmacy model registered

--- application/core/admin.py
+++ application/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from core.models import (
     Order, OrderItem, User, 
-    City, Medicine)
+    City, Medicine, Pharmacy)
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -20,9 +20,14 @@ class MedicineAdmin(admin.ModelAdmin):
     list_display = ['name']
     search_fields = ['name']
 
+class PharmacyAdmin(admin.ModelAdmin):
+    list_display = ['']
+    search_fields = ['']
+
 
 
 admin.site.register(Order, OrderAdmin)
 admin.site.register(User)
 admin.site.register(City, CityAdmin)
 admin.site.register(Medicine, MedicineAdmin)
+admin.site.register(Pharmacy,PharmacyAdmin)
