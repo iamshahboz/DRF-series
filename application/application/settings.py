db9b1d696191cbd5bb5606c15b788168b0d3a5df
Shahboz <sharafzodashahboz@gmail.com>
2025-02-26 04:02:47 +0500

    corrected the wrong placement of settings

--- application/application/settings.py
+++ application/application/settings.py
@@ -126,9 +126,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
         
-    ]
+        
+    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 # Simple jwt default settings 
