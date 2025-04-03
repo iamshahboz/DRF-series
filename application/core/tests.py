8138ed2b9501060fa7934a7daa23424ecd238047
Shahboz <sharafzodashahboz@gmail.com>
2025-04-04 04:52:18 +0500

    more tests added

--- application/core/tests.py
+++ application/core/tests.py
@@ -1,6 +1,7 @@
 from django.urls import reverse 
 from core.models import User, Product 
 from rest_framework.test import APITestCase
+from rest_framework import status
 
 # Create tests here 
 
@@ -19,6 +20,32 @@ class ProductAPITestCase(APITestCase):
 
     def test_get_product(self):
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], self.product.name)
+
+    def test_unauthorized_update_product(self):
+        data = {'name': 'Updated Product'}
+        response = self.client.put(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_unauthorized_delete_product(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+
+    def test_only_admins_can_delete_product(self):
+        # test normal user can not delete the product
+        self.client.login(username='user',password='userpass')
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(Product.objects.filter(pk=self.product.pk).exists())
+        
+        # test admin user can delete 
+        self.client.login(username='admin',password='adminpass')
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Product.objects.filter(pk=self.product.pk).exists())
+
+
+
+
 
